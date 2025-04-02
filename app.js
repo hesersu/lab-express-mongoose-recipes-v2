f6d084c278f5c5b1595b43794063b96011a6733d
Krists Krievins <krists.krievins@gmail.com>
2025-04-02 14:50:13 +0200

    Completed all Tasks

--- app.js
+++ app.js
@@ -8,44 +8,112 @@ app.use(logger("dev"));
 app.use(express.static("public"));
 app.use(express.json());
 
-
 // Iteration 1 - Connect to MongoDB
 // DATABASE CONNECTION
 
+const mongoose = require("mongoose");
+const recipeModel = require("./models/Recipe.model");
 
+const MONGODB_URI = "mongodb://127.0.0.1:27017/express-mongoose-recipes-dev";
+
+mongoose
+  .connect(MONGODB_URI)
+  .then((x) =>
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  )
+  .catch((err) => console.error("Error connecting to mongo", err));
 
 // ROUTES
 //  GET  / route - This is just an example route
-app.get('/', (req, res) => {
-    res.send("<h1>LAB | Express Mongoose Recipes</h1>");
+app.get("/", (req, res) => {
+  res.send("<h1>LAB | Express Mongoose Recipes</h1>");
 });
-
 
 //  Iteration 3 - Create a Recipe route
 //  POST  /recipes route
 
+app.post("/recipes", (req, res) => {
+  recipeModel
+    .create(req.body)
+    .then((createdRecipe) => {
+      console.log("Recipe created", createdRecipe);
+      res.status(201).json(createdRecipe);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ errorMessage: "Problem creating recipe" });
+    });
+});
 
 //  Iteration 4 - Get All Recipes
 //  GET  /recipes route
 
+app.get("/recipes", (req, res) => {
+  recipeModel
+    .find({})
+    .then((allRecipes) => {
+      console.log("Retrieved recipes", allRecipes);
+      res.status(200).json(allRecipes);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: "Failed request" });
+    });
+});
 
 //  Iteration 5 - Get a Single Recipe
 //  GET  /recipes/:id route
 
+app.get("/recipes/:id", (req, res) => {
+  const { id } = req.params;
+  recipeModel
+    .findById(id)
+    .then((oneRecipe) => {
+      console.log("Retrieved recipes", oneRecipe);
+      res.status(200).json(oneRecipe);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: "Failed request" });
+    });
+});
 
 //  Iteration 6 - Update a Single Recipe
 //  PUT  /recipes/:id route
 
+app.put("/recipes/:id", (req, res) => {
+  const { id } = req.params;
+  recipeModel
+    .findByIdAndUpdate(id, req.body, {
+      new: true,
+    })
+    .then((updatedRecipe) => {
+      console.log("Here are the recipes", updatedRecipe);
+      res.status(200).json(updatedRecipe);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ errorMessage: "There was error updating" });
+    });
+});
 
 //  Iteration 7 - Delete a Single Recipe
 //  DELETE  /recipes/:id route
 
-
+app.delete("/recipes/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deletedPet = await recipeModel.findByIdAndDelete(id);
+    console.log("Here is the deleted pet", deletedPet);
+    res.status(200).json(deletedPet);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ errorMessage: "There was an error deleting" });
+  }
+});
 
 // Start the server
-app.listen(3000, () => console.log('My first app listening on port 3000!'));
-
-
+app.listen(3000, () => console.log("My first app listening on port 3000!"));
 
 //❗️DO NOT REMOVE THE BELOW CODE
 module.exports = app;
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,1 +1,41 @@
 // Your code here ...
+
+// ./models/Recipe.model.js
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// CREATE SCHEMA
+// Schema - describes and enforces the structure of the documents
+const recipeSchema = new Schema({
+  title: { type: String, unique: true, required: true },
+  instructions: { type: String, required: true },
+  level: {
+    type: String,
+    enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"],
+  },
+  ingredients: { type: [String] },
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg",
+  },
+  duration: { type: Number, min: 0 },
+  isArchived: { type: Boolean, default: false },
+  created: { type: Date, default: Date.now },
+
+  //   title: String,
+  //   year: Number,
+  //   codeISBN: { type: String, maxlength: 13, unique: true },
+  //   quantity: { type: Number, min: 0, default: 0 },
+  //   lastPublished: { type: Date, default: Date.now },
+  //   genre: { type: String, enum: ["romance", "fiction", "biography", "poetry"] },
+  //   author: String,
+});
+
+// CREATE MODEL
+// The model() method defines a model (Book) and creates a collection (books) in MongoDB
+// The collection name will default to the lowercased, plural form of the model name:
+//                          "Book" --> "books"
+const Recipe = mongoose.model("Recipe", recipeSchema);
+
+// EXPORT THE MODEL
+module.exports = Recipe;
